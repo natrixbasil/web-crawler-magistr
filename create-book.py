e5a732d8ed560423e7deea5edbba51d7934fc8c0
natrixbasil <v.grosheva.sas@gmail.com>
2023-07-14 01:38:16 +0500

    chapter names and smaller font

---
+++ create-book.py
@@ -0,0 +1,134 @@
+from bs4 import BeautifulSoup
+import requests
+from ebooklib import epub
+
+chap_list = []
+for i in range(1, 7):
+    url = f'https://younettranslate.com/projects/magistr-dyavolskogo-kulta?page={i}'
+    response = requests.get(url)
+    html_content = response.content
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    div_element = soup.find('tbody')  # Replace 'your-div-id' with the actual ID of the div
+
+    if div_element:
+        # Find all <a> tags within the div
+        link_elements = div_element.find_all('a')
+
+        # Extract the link URLs and add them to the list
+        for link in link_elements:
+            chapter = link.get_text()
+            chap_list.append(chapter)
+
+chap_list2 = chap_list[::2]
+
+print(chap_list2[0:10])
+print(len(chap_list2))
+
+# Create an ePub book
+book = epub.EpubBook()
+
+# Set the metadata
+book.set_title('Мосян Тунсю - Магистр Дьявольского Культа')
+book.set_language('en')
+
+spine = []  # Create an empty list to store the chapters for the spine
+
+for chapter_name in chap_list2:
+    # Define the filename based on the chapter name
+    filename = f"{chapter_name.replace(' ', '_').lower()}.html"  # Adjust the filename format as desired
+
+    # Read the content from the file
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    # Create an ePub item for each chapter
+    chapter = epub.EpubHtml(title=chapter_name, file_name=filename, content=content)
+
+    # Add the chapter to the book
+    book.add_item(chapter)
+
+    # Add the chapter to the spine
+    spine.append(chapter)
+
+# Set the table of contents
+book.toc = spine
+
+# Set the order of items in the spine
+book.spine = spine
+
+# Add navigation files
+book.add_item(epub.EpubNcx())
+book.add_item(epub.EpubNav())
+
+# Set the CSS styles
+style = '''
+    body {
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+    }
+'''
+
+# Add CSS to the book
+book.add_item(epub.EpubItem(uid="style_default", file_name="style/default.css", content=style))
+
+# Set the CSS for the chapters
+for chapter in book.get_items_of_type(epub.EpubHtml):
+    chapter.add_link(href="style/default.css", rel="stylesheet", type="text/css")
+
+# Generate the ePub file
+epub.write_epub('Мосян_Тунсю_Магистр_Дьявольского_Культа.epub', book, {})
+
+
+# # Create an ePub book
+# book = epub.EpubBook()
+#
+# # Set the metadata
+# book.set_title('Мосян Тунсю - Магистр Дьявольского Культа')
+# book.set_language('en')
+#
+# for chapter_name in chap_list2:
+#     # Define the filename based on the chapter name
+#     filename = f"{chapter_name.replace(' ', '_').lower()}.html"
+#
+#     # Read the content from the file
+#     with open(filename, 'r', encoding='utf-8') as file:
+#         content = file.read()
+#         print(len(content))
+#
+#     # Create an ePub item for each chapter
+#     chapter = epub.EpubHtml(title=chapter_name, file_name=filename, content=content)
+#
+#     # Add the chapter to the book
+#     book.add_item(chapter)
+#
+#     # Add the chapter to the table of contents
+#     book.toc.append(chapter)
+#
+# # Add navigation files
+# book.add_item(epub.EpubNcx())
+# book.add_item(epub.EpubNav())
+#
+# # Set the CSS styles
+# style = '''
+#     body {
+#         font-family: Arial, sans-serif;
+#         font-size: 21px;
+#         line-height: 1.5;
+#     }
+# '''
+#
+# # Add CSS to the book
+# book.add_item(epub.EpubItem(uid="style_default", file_name="style/default.css", content=style))
+#
+# # Set the CSS for the chapters
+# for chapter in book.get_items_of_type(epub.EpubHtml):
+#     chapter.add_link(href="style/default.css", rel="stylesheet", type="text/css")
+#
+# # Define the order of items
+# book.spine = book.get_items_of_type(epub.EpubHtml)
+#
+# # Generate the ePub file
+# epub.write_epub('Мосян_Тунсю_Магистр_Дьявольского_Культа.epub', book, {})
--- get-urls.py
+++ get-urls.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-from ebooklib import epub
 
 link_list = []
+chap_list = []
 for i in range(1, 7):
     url = f'https://younettranslate.com/projects/magistr-dyavolskogo-kulta?page={i}'
     response = requests.get(url)
@@ -19,13 +19,19 @@ for i in range(1, 7):
         # Extract the link URLs and add them to the list
         for link in link_elements:
             href = link.get('href')
+            chapter = link.get_text()
             link_list.append(href)
+            chap_list.append(chapter)
 
+chap_list2 = chap_list[::2]
 link_list2 = link_list[::2]
-print('Number of chapters')
-print(len(link_list2))
 
-all_text =''
+print(chap_list2[0:10])
+print(link_list2[0:10])
+print(len(link_list2))
+print(len(chap_list2))
+
+all_text = []
 for link in link_list2:
     url = link
     response = requests.get(url)
@@ -34,50 +40,16 @@ for link in link_list2:
     soup = BeautifulSoup(html_content, 'html.parser')
     text = soup.find_all(class_='postdata-content')
     text = str(text)
-    all_text += text
+    all_text.append(text)
     print(len(text))
 
 print(len(all_text))
 
-# Create an ePub book
-book = epub.EpubBook()
+for chapter_name, chapter_content in zip(chap_list2, all_text):
+    # Define the filename based on the chapter name
+    filename = f"{chapter_name.replace(' ', '_').lower()}.html"
 
-# Set the metadata
-book.set_title('Магистр Дьявольского Культа')
-book.set_language('en')
-
-# Create an ePub item
-content = "<html><body>" + all_text + "</body></html>"
-chapter = epub.EpubHtml(title='Chapter 1', file_name='chapter1.xhtml', content=content)
-
-# Add the chapter to the book
-book.add_item(chapter)
-
-# Create the table of contents
-book.toc = (chapter,)  # Tuple of items for the table of contents
-
-# Add navigation files
-book.add_item(epub.EpubNcx())
-book.add_item(epub.EpubNav())
-
-# Define the CSS styles
-style = '''
-    body {
-        font-family: Arial, sans-serif;
-        font-size: 25px;
-        line-height: 1.5;
-    }
-'''
-
-# Add CSS to the book
-book.add_item(epub.EpubItem(uid="style_default", file_name="style/default.css", content=style))
-
-# Set the CSS for the chapter
-chapter.add_link(href="style/default.css", rel="stylesheet", type="text/css")
-
-# Define the order of items
-book.spine = [chapter]
-
-# Generate the ePub file
-epub.write_epub('my_ebook.epub', book, {})
+    # Write the text content to the file
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(chapter_content)
 
